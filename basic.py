70b969628d4e3d574c8b3f87b220de2a8c668d88
pihani patel <pihanipatel982005@gmail.com>
2026-07-06 20:53:27 +0530

    first project added

---
+++ basic.py
@@ -0,0 +1,53 @@
+# print("pih",end=" ")
+
+# name=str(input("what is your name"))
+# age=int(input("what is your age"))
+# favourite_hobby=str(input("your favourite_hobby"))
+# print(f"Hello {name}! At {age}, enjoying {favourite_hobby} sounds fun!") 
+
+
+# n=20
+# m= 2.2
+# str="ppi"
+# bool=True
+# complex=20
+
+# print(type(n),n)
+# print(type(m),m)
+# print(type(str),str)
+# print(type(bool),bool)
+# print(type(complex),complex)
+
+
+
+
+# height = int(input("enter your height"))
+# weight = int(input("enter your weight"))
+
+# print(f"your {height} and {weight} is matched to criteria")
+# b = False
+# age = bool(input("your age is abouve 18?"))
+# id = bool(input("do you have id?"))
+# print(age or id)
+# n=3
+# n+=2
+# print(n)
+
+# name="123"
+# x=int(name)
+# print(type(x),x)
+
+# num = float(input("enter floting point number"))
+# x=int(num)
+# print(f"diffrence {type(num)},{type(x)}")
+
+# age = bool(input("above 18?"))
+# x=str(age)
+# y=int(age)
+
+# print(f"all three values {age},{x},{y}")
+# print(id(x))
+
+#x=7
+#y=7
+#print(id(x),id(y))
